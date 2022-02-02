7be1fcbfd0c8347de6431ab25dd0dc290f474118
offiahchiaz <offiahchiaz@gmail.com>
2022-02-02 16:46:24 +0100

    Added SingleCard component for displaying one card

---
+++ src/components/SingleCard.js
@@ -0,0 +1,20 @@
+import './Singlecard.css'
+
+export const SingleCard =({ card, handleChoice }) => {
+
+  const handleClick = () => {
+    handleChoice(card)
+  }
+
+  return (
+    <div className="card">
+      <div>
+        <img className='front' src={card.src} alt="card front" />
+        <img className='back' onClick={handleClick} src="/img/cover.png" alt="card back" />
+      </div>
+    </div>     
+   
+  );
+}
+
+
