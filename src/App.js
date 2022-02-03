674549b20037f201f5344a4353e2b0dd84d21f47
offiahchiaz <offiahchiaz@gmail.com>
2022-02-03 10:28:46 +0100

    Added counter to display how many time card selections it took for the player to finish the game

--- src/App.js
+++ src/App.js
@@ -25,6 +25,8 @@ function App() {
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }))
 
+    setChoiceOne(null)
+    setChoiceTwo(null)  
     setCards(shuffledCards)
     setTurns(0)
   }
@@ -39,7 +41,7 @@ function App() {
   useEffect(() => {
     if (choiceOne && choiceTwo) {
       setDisabled(true)
-      
+
       if (choiceOne.src === choiceTwo.src) {
         setCards(prevCards => {
           return prevCards.map(card => {
@@ -66,6 +68,11 @@ function App() {
     setDisabled(false)
   }
 
+  // start new game automatically
+  useEffect(() => {
+    shuffleCards()
+  }, [])
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
@@ -84,6 +91,7 @@ function App() {
           ))
         }
       </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
